662ea96cfe158aa275ade99e20aeb1412859df7b
MinerAle <alessandro.p3000@gmail.com>
2022-05-03 22:57:34 +0200

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,4 +1,12 @@
 #!bin/bash
 #https://github.com/MinerAle00/proxmox-utility
 
-echo "This script his made by MinerAle and PelliD"
+x=0
+
+
+
+echo "Questo script è stato scritto da MinerAle e PellinD"
+sleep 1
+echo "Gli autori dello script non rispondono su eventuali errori!"
+sleep 2
+echo "Seleziona il programma che vuoi utilizzare: "input (x)
