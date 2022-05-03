0b72d77181f47de263442c153a1613a3d2a303ee
MinerAle <alessandro.p3000@gmail.com>
2022-05-03 23:21:48 +0200

    Update script.sh

--- script.sh
+++ script.sh
@@ -9,4 +9,4 @@ echo "Questo script è stato scritto da MinerAle e PellinD"
 sleep 1
 echo "Gli autori dello script non rispondono su eventuali errori!"
 sleep 2
-echo "Seleziona il programma che vuoi utilizzare: "input (x)
+echo "Seleziona il programma che vuoi utilizzare: "input (x);
